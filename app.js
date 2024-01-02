7c9484de41ed0d2b2176286100e25f6b8bf21715
Jerry S. Nawerth V. <123263613+jsnawerth@users.noreply.github.com>
2024-01-02 15:25:29 +0100

    Added connection with database

--- app.js
+++ app.js
@@ -1,10 +1,22 @@
 const express = require('express');
+const bodyParser = require('body-parser')
 const app = express();
-const port = 1993;
+const port = 3000;
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-});
+const db = require('./db/index.js')
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.get('/', (request, response) => {
+  response.json({ info: 'Node.js, Express, and Postgres API' })
+})
+
+app.get('/users', db.getUsers);
+app.get('/users/:id', db.getUserById)
+app.post('/users', db.createUser)
+app.put('/users/:id', db.updateUser)
+app.delete('/users/:id', db.deleteUser)
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
